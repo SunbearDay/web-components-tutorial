53896744b9d6ec9892406533766bd76f3b53baaf
sunbear <sunbearday@outlook.com>
2023-08-16 15:21:13 -0700

    save

--- bigbang.js
+++ bigbang.js
@@ -3,8 +3,42 @@
 
 const template = document.createElement('template')
 template.innerHTML = `
+    <style>
+        /* We can import stylesheets! */
+        /* import url(); */
+
+        div {
+            /* border: 1px solid red; */
+            padding: 3rem;
+            margin: 3rem;
+        }
+
+        :host {
+            /* For the shadow root. */
+            background-color: lavender;
+            display: block;
+        }
+
+        :host(big-bang) {
+            /* Apply only for the specified element. */
+            background-color: gold;
+        }
+
+        :host-context(main) {
+            /* Not available in Firefox yet. */
+            background-color: gold;
+        }
+
+        ::slotted(h2) {
+            font-size: 4rem;
+        }
+
+        ::part() {
+            /* Does not work here. */
+        }
+    </style>
     <div>
-    <slot name="list"></slot>
+        <slot name="list"></slot>
         <h1>Big Bang Theory</h1>
         <slot name="title">
             Default text if no title used in HTML.
