b715d9d904095ea3faa76d8d174f4bf4ec36ff08
sunbear <sunbearday@outlook.com>
2023-08-16 16:22:01 -0700

    save

--- bigbang.js
+++ bigbang.js
@@ -4,41 +4,34 @@
 const template = document.createElement('template')
 template.innerHTML = `
     <style>
-        /* We can import stylesheets! */
-        /* import url(); */
-
-        div {
-            /* border: 1px solid red; */
-            padding: 3rem;
-            margin: 3rem;
-        }
-
         :host {
-            /* For the shadow root. */
-            background-color: lavender;
-            display: block;
-        }
-
-        :host(big-bang) {
-            /* Apply only for the specified element. */
-            background-color: gold;
-        }
-
-        :host-context(main) {
-            /* Not available in Firefox yet. */
-            background-color: gold;
+            /* The shadow root. */
+            background-color: #333; /* default */
+            color: white;
+            display: block; /* critical */
         }
 
         ::slotted(h2) {
-            font-size: 4rem;
+            /* Represents an h2 element that has been placed into a slot */
+            font-weight: 300;
         }
 
-        ::part() {
-            /* Does not work here. */
+        .root {
+            position: relative;
+            padding: 2rem;
+        }
+
+        .character {
+            position: absolute;
+            z-index: 10;
+            top: -10rem;
+            left: 0;
+            font-size: 10rem;
+            line-height: 1;
+            color: hsla(60 50% 80% / 0.32);
         }
     </style>
-    <div>
-        <slot name="list"></slot>
+    <div class="root">
         <h1>Big Bang Theory</h1>
         <slot name="title">
             Default text if no title used in HTML.
@@ -49,14 +42,51 @@ template.innerHTML = `
 class BigBang extends HTMLElement {
     constructor() {
         super()
-        const shadowRoot = this.attachShadow({ mode: 'closed' })
-
-        // let div = document.createElement('div')
-        // div.textContent = 'Big Bang Theory'
-        // shadowRoot.append(div)
+        this.root = this.attachShadow({ mode: 'closed' })
 
         let clone = template.content.cloneNode(true)
-        shadowRoot.append(clone)
+        this.root.append(clone)
+    }
+
+    // Define the allowed attributes.
+    static get observedAttributes() {
+        // Belongs to the HTMLElement.
+        // Return the attributes we want to observe.
+        return ['character', 'color']
+    }
+
+    // Sync attributes with properties as you want.
+    get character() {
+        return this.getAttribute('character')
+    }
+
+    set character(value) {
+        this.setAttribute('character', value)
+    }
+
+    get color() {
+        return this.getAttribute('color')
+    }
+
+    set color(value) {
+        this.setAttribute('color', value)
+    }
+
+    // Handle values and changes to the attributes.
+    attributeChangedCallback(attrName, oldVal, newVal) {
+        if (attrName === 'character') {
+            const div = this.root.querySelector('.root')
+            let p = div.querySelector('p')
+                ? div.querySelector('p')
+                : document.createElement('p')
+            p.className = 'character'
+            p.textContent = newVal
+            div.append(p)
+        }
+
+        if (attrName === 'color') {
+            this.style.backgroundColor = newVal
+        }
     }
 }
 
--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log('main.js loaded.')
+
+const bb = document.querySelector('big-bang')
+bb.addEventListener('click', changeCharacter)
+
+function changeCharacter(event) {
+    // User clicked on <big-bang> element.
+    const bb = event.target
+    bb.character = bb.character === 'Leonard' ? 'Sheldon' : 'Leonard'
+
+    bb.color = bb.color === 'cornflowerblue' ? 'lightcoral' : 'cornflowerblue'
+}
