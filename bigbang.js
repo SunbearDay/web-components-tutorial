4987d66bc59daada8a6ca716ebc40fe38334f26e
sunbear <sunbearday@outlook.com>
2023-08-16 09:49:34 -0700

    save

---
+++ bigbang.js
@@ -0,0 +1,29 @@
+// This file will be the web component.
+// It only needs to run, not be imported by main.js.
+
+const template = document.createElement('template')
+template.innerHTML = `
+    <div>
+    <slot name="list"></slot>
+        <h1>Big Bang Theory</h1>
+        <slot name="title">
+            Default text if no title used in HTML.
+        </slot>
+    </div>
+`
+
+class BigBang extends HTMLElement {
+    constructor() {
+        super()
+        const shadowRoot = this.attachShadow({ mode: 'closed' })
+
+        // let div = document.createElement('div')
+        // div.textContent = 'Big Bang Theory'
+        // shadowRoot.append(div)
+
+        let clone = template.content.cloneNode(true)
+        shadowRoot.append(clone)
+    }
+}
+
+window.customElements.define('big-bang', BigBang)
